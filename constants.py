abff341d158056065150f41bbd80f619e0ac7ce6
Kevin Gonzalez <137135509+gonzalez-kevin@users.noreply.github.com>
2025-12-14 16:29:23 -0800

    "connecting to db"

--- constants.py
+++ constants.py
@@ -1,2 +1,18 @@
+# league information
 current_season = '2025-26'
 league_id = '00'
+season_type = 'Regular Season'
+
+# PLAYER AVERAGES INFORMATION
+# team information
+team_info = ['TEAM_ID', 'TEAM_ABBREVIATION']
+
+# player info
+player_info = ['PLAYER_ID', 'PLAYER_NAME']
+
+#stats to include
+included_stats = [
+    'GP', 'MIN', 'FGM', 'FGA', 'FG3M', 'FG3A', 'FG3_PCT',
+    'FTM', 'FTA', 'FT_PCT', 'REB', 'AST', 'TOV', 'STL',
+    'BLK', 'PTS'
+    ]
